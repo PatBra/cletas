8430ec80d5102a200495faa47d331744485fc4e2
garaos <g.araos@gmail.com>
2023-02-10 22:48:21 -0300

    carga de productos

--- frontend/src/components/products/Product.jsx
+++ frontend/src/components/products/Product.jsx
@@ -1,35 +1,35 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
-const Product =( {product}) => {
-return (
-    
-<div  className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                                <Card style={{ width: '18rem' }}>
-                                    <Card.Img variant="top" src="https://devinci-media-prod.azureedge.net/media/amvgpag4/wilson_2022_v2.png?center=0.5,0.5&mode=max&bgcolor=FFFFFF&format=jpeg&width=1800&height=768" />
-                                    <Card.Body>
-                                        <img src={product.images[0].url} alt="wilson" className='card-img-top mx-auto' />
-                                        <Card.Title><a>{product.name}</a></Card.Title>
-                                        <Card.Text>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ut dignissimos voluptatem reprehenderit nobis quo velit, placeat nostrum alias itaque totam deserunt rerum veniam magni porro dolorum provident excepturi eligendi.
-                                        </Card.Text>
-                                        <Button variant="primary">Ver</Button>
-                                        <Button variant="primary">Agregar al Carrito</Button>
-                                        <div className='rating mt-auto'>
-                                            <div className='rating-outer'>
-                                                <div className='rating-inner'></div>
-                                            </div>
-                                            <span id='no_of_reviews'>({product.numOfReviews})</span>
-                                        </div>
-                                        <Card.Text>
-                                            <p>${product.price}</p>
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-    
-)
+const Product = ({ product }) => {
+    return (
+
+        <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="https://devinci-media-prod.azureedge.net/media/amvgpag4/wilson_2022_v2.png?center=0.5,0.5&mode=max&bgcolor=FFFFFF&format=jpeg&width=1800&height=768" />
+                <Card.Body>
+                    <img src={product.images[0].url} alt="wilson" className='card-img-top mx-auto' />
+                    <Card.Title><Link to={`/product/${product._id}`} >{product.name}</Link></Card.Title>
+                    <Card.Text>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ut dignissimos voluptatem reprehenderit nobis quo velit, placeat nostrum alias itaque totam deserunt rerum veniam magni porro dolorum provident excepturi eligendi.
+                    </Card.Text>
+                    <Link to={`/product/${product._id}`} id="view_btn" className="btn btn-block">Ver</Link>
+                    <div className='rating mt-auto'>
+                        <div className='rating-outer'>
+                            <div className='rating-inner'></div>
+                        </div>
+                        <span id='no_of_reviews'>({product.numOfReviews})</span>
+                    </div>
+                    <Card.Text>
+                        <p>${product.price}</p>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+
+    )
 }
 
 export default Product
--- frontend/src/contents/Home.jsx
+++ frontend/src/contents/Home.jsx
@@ -4,6 +4,7 @@ import MetaData from '../components/layout/MetaData';
 import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../actions/product.Actions'
 import Product from '../components/products/Product';
+import { Fragment } from 'react';
 
 const Home = () => {
     const dispatch = useDispatch()
@@ -14,30 +15,34 @@ const Home = () => {
     }, [dispatch])
 
     return (
-        <div>
-            <MetaData title={`El mega titulo`} />
+        <Fragment>
+            {loading ? <h1>Cargando...</h1> : (
+                <div>
+                    <MetaData title={`El mega titulo`} />
+                    <div className='row jumbito'>
+                        <div className='col-12 col-md-9 col-lg-7 col-xl-6'>
+                            <div className='container-fluid py-5 mt-5 text-black-50 px-3'>
+                                <h1 className='display-5 fw-bold'>Casual y Técnico</h1>
+                                <p className='col-md-8 fs-4'>Una bicicleta para cada momento de tu día</p>
+                                <p className='fs-5 fw-bolder'>Descubre más <i className='fa-solid fa-skull'></i></p>
+                            </div>
+                        </div>
+                    </div>
 
-            <div className='row jumbito'>
-                <div className='col-12 col-md-9 col-lg-7 col-xl-6'>
-                    <div className='container-fluid py-5 mt-5 text-black-50 px-3'>
-                        <h1 className='display-5 fw-bold'>Casual y Técnico</h1>
-                        <p className='col-md-8 fs-4'>Una bicicleta para cada momento de tu día</p>
-                        <p className='fs-5 fw-bolder'>Descubre más <i className='fa-solid fa-skull'></i></p>
+                    <div>
+                        <h1 className='text-center fw-bold pt-5' id='bicicletas'>Bicicletas</h1>
+                        <section id='products' className='container mt-5'>
+                            <div className='row'>
+                                {products && products.map(product => (
+                                    <Product key={product._id} product={product} />
+                                ))}
+                            </div>
+                        </section>
                     </div>
                 </div>
-            </div>
+            )}
+        </Fragment>
 
-            <div>
-                <h1 className='text-center fw-bold pt-5' id='bicicletas'>Bicicletas</h1>
-                <section id='products' className='container mt-5'>
-                    <div className='row'>
-                        {products && products.map(product => (
-                            <Product  key={product._id} product={product} />
-                        ))}
-                    </div>
-                </section>
-            </div>
-        </div>
     );
 }
 
